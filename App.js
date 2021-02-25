5cb036a932ae6237206466f4003e3d745e8ef9dd
ashwani100596 <ashwan201kumar@gmail.com>
2021-02-25 20:34:24 +0530

    Login Page

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
+ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+
 
 export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#ecf0f1',
+    },
+    inputText: {
+      width: 200,
+      height: 44,
+      padding: 10,
+      borderWidth: 1,
+      borderColor: 'black',
+      marginBottom: 10,
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <TextInput
+    placeholder={'Username'}
+    style={styles.inputText}
+     />
+    <TextInput 
+      placeholder={'Password'}
+      style={styles.inputText}
+    />
+    <Button
+         title={'Login'}
+          style={styles.inputText}
+          // onPress={this.onLogin.bind(this)}
+        />
+      <Text>Hello Mr. Ashwani kumar how was the meeting of today</Text>
       <StatusBar style="auto" />
     </View>
   );
