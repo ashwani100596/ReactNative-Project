7d8992cf5a3ca9d890194f3f1c13cb0100a187cf
ashwani100596 <ashwan201kumar@gmail.com>
2021-03-01 19:43:13 +0530

    Splassh screen added

--- App.js
+++ App.js
@@ -1,43 +1,52 @@
- import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
-
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import Constants from "expo-constants";
+import { StatusBar } from "expo-status-bar";
+import Head from "./head";
 
 export default function App() {
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#ecf0f1',
-    },
-    inputText: {
-      width: 200,
-      height: 44,
-      padding: 10,
-      borderWidth: 1,
-      borderColor: 'black',
-      marginBottom: 10,
-    },
-  });
+  const [splash, setSplash] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setSplash(false);
+    }, 3000);
+  }, []);
 
-  return (
+  return splash ? (
     <View style={styles.container}>
-    <TextInput
-    placeholder={'Username'}
-    style={styles.inputText}
-     />
-    <TextInput 
-      placeholder={'Password'}
-      style={styles.inputText}
-    />
-    <Button
-         title={'Login'}
-          style={styles.inputText}
-          // onPress={this.onLogin.bind(this)}
-        />
-      <Text>Hello Mr. Ashwani kumar how was your day</Text>
+      <Image style={styles.logo} source={require("./assets/blur.jpg")} />
+    </View>
+  ) : (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>Hello Welcome to CrownStack</Text>
+      <Head />
+      <Text>Thank you for giving your valuable time</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  inputText: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+  },
+});
---
+++ head.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Text, View, TextInput, Button, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
+export default function Head() {
+  return (
+    <View style={styles.container}>
+      <TextInput placeholder={"xyz@gmail.com"} style={styles.inputText} />
+      <TextInput placeholder={"Password"} style={styles.inputText} />
+      <Button title={"Login"} style={styles.inputText} />
+      <Text>hello ashwani you are awesome yar</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  inputText: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+  },
+});
