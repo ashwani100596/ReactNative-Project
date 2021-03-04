deddf88211bc1601dd3c75ded34d320f6e6ec947
ashwani100596 <ashwan201kumar@gmail.com>
2021-03-04 20:47:18 +0530

    Navigation

--- head.js
+++ head.js
@@ -1,15 +1,72 @@
 import React from "react";
-import { Text, View, TextInput, Button, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
 import Constants from "expo-constants";
+import { StatusBar } from "expo-status-bar";
+// import App from "/.navigation/Home.js";
+// import Example from "./Login.js";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen hello</Text>
+      <Button
+        title="Go to details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+function Details() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Hello how are you</Text>
+    </View>
+  );
+}
+const Stack = createStackNavigator();
 
 export default function Head() {
   return (
-    <View style={styles.container}>
-      <TextInput placeholder={"xyz@gmail.com"} style={styles.inputText} />
-      <TextInput placeholder={"Password"} style={styles.inputText} />
-      <Button title={"Login"} style={styles.inputText} />
-      <Text>CrownStack welcome you</Text>
-    </View>
+    <NavigationContainer initialRoutename="Home">
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Home" }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{ title: "Details" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <ImageBackground
+    //     source={require("./assets/blur.jpg")}
+    //     style={{ width: "100%", height: "100%" }}
+    //   >
+    //     {/* <App /> */}
+    //     <Text style={styles.paragraph}>Hello Welcome to The CrownStack</Text>
+    //     {/* <Example /> */}
+    //     {/* <TextInput placeholder={"xyz@gmail.com"} style={styles.inputText} />
+    //     <TextInput placeholder={"Password"} style={styles.inputText} />
+    //     <Button title={"Login"} style={styles.inputText} /> */}
+    //     <Text style={styles.paragraph}>CrownStack</Text>
+    //     {/* <Text style={styles.paragraph}>Thanks to all </Text> */}
+    //     <StatusBar style="auto" />
+    //   </ImageBackground>
+    // </View>
   );
 }
 const styles = StyleSheet.create({
@@ -28,5 +85,18 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "black",
     marginBottom: 10,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+  },
+  hello: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
