24404e14d4198509021f24b77ec89dde62d424fd
ashwani100596 <ashwan201kumar@gmail.com>
2021-03-08 20:05:25 +0530

    Login page after Splash Scree

--- head.js
+++ head.js
@@ -10,63 +10,71 @@ import {
 import Constants from "expo-constants";
 import { StatusBar } from "expo-status-bar";
 // import App from "/.navigation/Home.js";
-// import Example from "./Login.js";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./Login";
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen hello</Text>
-      <Button
-        title="Go to details"
-        onPress={() => navigation.navigate("Details")}
-      />
-    </View>
-  );
-}
+// function HomeScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Text>Home Screen hello</Text>
+//       <Button title="Details" onPress={() => navigation.navigate("Details")} />
+//     </View>
+//   );
+// }
 
-function Details() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Hello how are you</Text>
-    </View>
-  );
-}
-const Stack = createStackNavigator();
+// function Details({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Text>Hello how are you</Text>
+//       <Text>Home Screen hello</Text>
+//       <Button
+//         title="Login your account"
+//         onPress={() => navigation.navigate("login")}
+//       />
+//     </View>
+//   );
+// }
+// const Stack = createStackNavigator();
 
 export default function Head() {
   return (
-    <NavigationContainer initialRoutename="Home">
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Home" }}
-        />
-        <Stack.Screen
-          name="Details"
-          component={Details}
-          options={{ title: "Details" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-    // <View style={styles.container}>
-    //   <ImageBackground
-    //     source={require("./assets/blur.jpg")}
-    //     style={{ width: "100%", height: "100%" }}
-    //   >
-    //     {/* <App /> */}
-    //     <Text style={styles.paragraph}>Hello Welcome to The CrownStack</Text>
-    //     {/* <Example /> */}
-    //     {/* <TextInput placeholder={"xyz@gmail.com"} style={styles.inputText} />
-    //     <TextInput placeholder={"Password"} style={styles.inputText} />
-    //     <Button title={"Login"} style={styles.inputText} /> */}
-    //     <Text style={styles.paragraph}>CrownStack</Text>
-    //     {/* <Text style={styles.paragraph}>Thanks to all </Text> */}
-    //     <StatusBar style="auto" />
-    //   </ImageBackground>
-    // </View>
+    // <NavigationContainer initialRoutename="Home">
+    //   <Stack.Navigator>
+    //     <Stack.Screen
+    //       name="Home"
+    //       component={HomeScreen}
+    //       options={{ title: "Home" }}
+    //     />
+    //     <Stack.Screen
+    //       name="Details"
+    //       component={Details}
+    //       options={{ title: "Details" }}
+    //     />
+    //     <Stack.Screen
+    //       name="LoginPage"
+    //       component={Login}
+    //       options={{ title: "LoginPage" }}
+    //     />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <View style={styles.container}>
+      {/* <ImageBackground
+        source={require("./assets/blur.jpg")}
+        style={{ width: "100%", height: "100%" }}
+      > */}
+      {/* <App /> */}
+      <Text style={styles.paragraph}>Hello Welcome to The CrownStack</Text>
+      <Login />
+      {/* <Example /> */}
+      {/* <TextInput placeholder={"xyz@gmail.com"} style={styles.inputText} />
+        <TextInput placeholder={"Password"} style={styles.inputText} />
+        <Button title={"Login"} style={styles.inputText} /> */}
+      <Text style={styles.paragraph}>CrownStack</Text>
+      {/* <Text style={styles.paragraph}>Thanks to all </Text> */}
+      <StatusBar style="auto" />
+      {/* </ImageBackground> */}
+    </View>
   );
 }
 const styles = StyleSheet.create({
