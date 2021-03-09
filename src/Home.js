5fa5885faec78ec6cea7394287bc9ca50cc05f12
ashwani100596 <ashwan201kumar@gmail.com>
2021-03-10 03:39:32 +0530

    Complete login

---
+++ src/Home.js
@@ -0,0 +1,110 @@
+import React from "react";
+import {
+  Text,
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
+import Constants from "expo-constants";
+import { StatusBar } from "expo-status-bar";
+// import App from "/.navigation/Home.js";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./src/Login";
+
+// function HomeScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Text>Home Screen hello</Text>
+//       <Button title="Details" onPress={() => navigation.navigate("Details")} />
+//     </View>
+//   );
+// }
+
+// function Details({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Text>Hello how are you</Text>
+//       <Text>Home Screen hello</Text>
+//       <Button
+//         title="Login your account"
+//         onPress={() => navigation.navigate("login")}
+//       />
+//     </View>
+//   );
+// }
+// const Stack = createStackNavigator();
+
+export default function Home() {
+  return (
+    // <NavigationContainer initialRoutename="Home">
+    //   <Stack.Navigator>
+    //     <Stack.Screen
+    //       name="Home"
+    //       component={HomeScreen}
+    //       options={{ title: "Home" }}
+    //     />
+    //     <Stack.Screen
+    //       name="Details"
+    //       component={Details}
+    //       options={{ title: "Details" }}
+    //     />
+    //     <Stack.Screen
+    //       name="LoginPage"
+    //       component={Login}
+    //       options={{ title: "LoginPage" }}
+    //     />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    // <View style={styles.container}>
+      {/* <ImageBackground
+        source={require("./assets/blur.jpg")}
+        style={{ width: "100%", height: "100%" }}
+      > */}
+      {/* <App /> */}
+      // <Text style={styles.paragraph}>Hello Welcome to The CrownStack</Text>
+      // <Login />
+      {/* <Example /> */}
+      {/* <TextInput placeholder={"xyz@gmail.com"} style={styles.inputText} />
+        <TextInput placeholder={"Password"} style={styles.inputText} />
+        <Button title={"Login"} style={styles.inputText} /> */}
+      // <Text style={styles.paragraph}>CrownStack</Text>
+      {/* <Text style={styles.paragraph}>Thanks to all </Text> */}
+      // <StatusBar style="auto" />
+      {/* </ImageBackground> */}
+    // </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  inputText: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+  },
+  hello: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ src/Login.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { Text, View, TextInput, Button, StyleSheet } from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import Constants from "expo-constants";
+
+export default function Login() {
+  const { control, handleSubmit, errors } = useForm();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>Hello Welcome to The CrownStack</Text>
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            style={{
+              paddingHorizontal: 20,
+              borderWidth: 1,
+              paddingVertical: 8,
+              margin: 15,
+              height: 10,
+              borderColor: "#7a42f4",
+              borderWidth: 1,
+            }}
+            style={styles.input}
+            placeholder="Enter Your Username"
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            value={value}
+          />
+        )}
+        name="UserName"
+        rules={{ required: true }}
+        defaultValue=""
+      />
+      {errors.UserName && <Text>User Name is required.</Text>}
+
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            style={styles.input}
+            secureTextEntry={true}
+            placeholder="Enter Your Password"
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            value={value}
+          />
+        )}
+        name="Password"
+        rules={{ required: true, minLength: 8 }}
+        defaultValue=""
+      />
+
+      {errors.Password?.type === "required" && (
+        <Text>Password is required.</Text>
+      )}
+
+      {errors.Password?.type === "minLength" && (
+        <Text>Minimum 8 characters are required</Text>
+      )}
+
+      <Button
+        style={styles.submitButtonText}
+        title="Submit"
+        onPress={handleSubmit((data) => console.log(data))}
+      />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "white",
+    padding: 8,
+  },
+  input: {
+    margin: 15,
+    width: 200,
+    height: 40,
+    borderColor: "#7a42f4",
+    borderWidth: 1,
+    borderRadius: 45,
+    paddingLeft: 45,
+  },
+  submitButton: {
+    backgroundColor: "#7a42f4",
+    padding: 10,
+    margin: 15,
+    height: 40,
+  },
+  submitButtonText: {
+    color: "white",
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "black",
+  },
+});
