0531819ee9e71e7931651c2dce8fee9acf87553a
ashwani100596 <ashwan201kumar@gmail.com>
2021-03-18 13:42:51 +0530

    MainStack

--- src/MainStackNavigator.js
+++ src/MainStackNavigator.js
@@ -1,85 +1,12 @@
 import React, { component } from "react";
 import { Text, View, Button, StyleSheet } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./BottomNavigation/homeScreen";
 import SettingsScreen from "./BottomNavigation/settingScreen";
 import Notification from "./BottomNavigation/notification";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import HomeStackNavigator from "./StackNavigation/HomeStackNavigator";
-// import { MyDrawer } from "./drawerContent";
-// import Main from "./StackNavigation/Main";
-// import Index from "./BottomNavigation/index";
-
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-// import {
-//   createDrawerNavigator,
-//   DrawerContentScrollView,
-//   DrawerItemList,
-//   DrawerItem,
-// } from "@react-navigation/drawer";
 
-// function Feed({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//       <Text>Feed Screen</Text>
-//       <Button
-//         title="Open drawer"
-//         onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-//       />
-//       <Button
-//         title="Toggle drawer"
-//         onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
-//       />
-//     </View>
-//   );
-// }
-
-// function Notifications() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//       <Text>Notifications Screen</Text>
-//     </View>
-//   );
-// }
-
-// function CustomDrawerContent(props) {
-//   return (
-//     <DrawerContentScrollView {...props}>
-//       <DrawerItemList {...props} />
-//       <DrawerItem
-//         label="Close drawer"
-//         onPress={() => props.navigation.dispatch(DrawerActions.closeDrawer())}
-//       />
-//       <DrawerItem
-//         label="Toggle drawer"
-//         onPress={() => props.navigation.dispatch(DrawerActions.toggleDrawer())}
-//       />
-//     </DrawerContentScrollView>
-//   );
-// }
-
-// const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
-// const Drawer = createDrawerNavigator();
-
-// const HomeStackNavigator = () => {
-//   return (
-//     <Stack.Navigator
-//       screenOptions={{
-//         headerStyle: {
-//           backgroundColor: "#9AC4F8",
-//         },
-//         headerTintColor: "white",
-//         headerBackTitle: "Back",
-//       }}
-//     >
-//       <Stack.Screen name="HomePage" component={HomePage} />
-//       <Stack.Screen name="Scroll" component={Scroll} />
-//     </Stack.Navigator>
-//   );
-// };
-
 const MainStackNavigator = () => {
   return (
     <Tab.Navigator
