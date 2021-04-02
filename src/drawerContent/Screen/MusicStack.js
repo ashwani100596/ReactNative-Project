020ef3f6db4c6b21ee8449bf86c8e9a7feefef2c
ashwani100596 <ashwan201kumar@gmail.com>
2021-04-02 19:37:06 +0530

    MusicStack

---
+++ src/drawerContent/Screen/MusicStack.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Button, View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import DetailS from "./Details";
+// import Music from "../component/MusicMusic";
+import Music from "./Music";
+import MusicList from "./MusicList";
+import Details from "./Details";
+
+const StackMusic = createStackNavigator();
+
+const MusicStack = () => {
+  return (
+    <StackMusic.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#9AC4F8",
+        },
+        headerTintColor: "white",
+        headerBackTitle: "Back",
+      }}
+    >
+      <StackMusic.Screen
+        name="MusicScreen"
+        component={MusicList}
+        options={{
+          title: "Micheal-Jackson Songs",
+          headerStyle: {
+            backgroundColor: "#fff",
+          },
+          headerTintColor: "black",
+        }}
+      />
+      <StackMusic.Screen name="Details" component={Details} />
+      <StackMusic.Screen name="Music" component={Music} />
+    </StackMusic.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+});
+
+export default MusicStack;
