9dac619f41f55f85b3a562b1f5822f9e6a39dc55
ashwani100596 <ashwan201kumar@gmail.com>
2021-04-02 19:45:29 +0530

    HomeStackNavigator

--- src/StackNavigation/HomeStackNavigator.js
+++ src/StackNavigation/HomeStackNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import HomePage from "./screens/HomePage";
+import HomePage from "../StackNavigation/screens/HomePage";
 import Scroll from "./screens/Scroll";
 
 const Stack = createStackNavigator();
