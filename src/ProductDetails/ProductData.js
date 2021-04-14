df132a471c401c02b67cb122fa120d4ad95d55e9
ashwani100596 <ashwan201kumar@gmail.com>
2021-04-14 21:02:07 +0530

    DummyData

--- src/ProductDetails/ProductData.js
+++ src/ProductDetails/ProductData.js
@@ -1,63 +1,115 @@
 const DummyData = {
-  details: {
-    id: "1",
-    ProductName: "Australian-Cherry",
-
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    Offerprice: "$10.5",
-    Actualprice: "$15.5",
-    first_pic: require("../../images/cherry1.jpg"),
-    second_pic: require("../../images/cherry2.jpg"),
-    third_pic: require("../../images/cherry3.jpg"),
-    fourth_pic: require("../../images/cherry4.jpg"),
-  },
-  //   {
-  //     id: "2",
-  //     ProductName: "Australian-Cherry",
-
-  //     description:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  //     price: "Rs.$10",
-  //     first_pic: require("../../images/cherry1.jpg"),
-  //     second_pic: require("../../images/cherry2.jpg"),
-  //     third_pic: require("../../images/cherry3.jpg"),
-  //     fourth_pic: require("../../images/cherry4.jpg"),
-  //   },
-  //   {
-  //     id: "3",
-  //     ProductName: "Australian-Cherry",
-  //     description:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  //     price: "Rs.$10",
-  //     first_pic: require("../../images/cherry1.jpg"),
-  //     second_pic: require("../../images/cherry2.jpg"),
-  //     third_pic: require("../../images/cherry3.jpg"),
-  //     fourth_pic: require("../../images/cherry4.jpg"),
-  //   },
-  //   {
-  //     id: "4",
-  //     ProductName: "Australian-Cherry",
-  //     description:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  //     price: "Rs.$10",
-  //     first_pic: require("../../images/cherry1.jpg"),
-  //     second_pic: require("../../images/cherry2.jpg"),
-  //     third_pic: require("../../images/cherry3.jpg"),
-  //     fourth_pic: require("../../images/cherry4.jpg"),
-  //   },
-  //   {
-  //     id: "5",
-  //     ProductName: "Australian-Cherry",
-
-  //     description:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  //     price: "Rs.$10",
-  //     first_pic: require("../../images/cherry1.jpg"),
-  //     second_pic: require("../../images/cherry2.jpg"),
-  //     third_pic: require("../../images/cherry3.jpg"),
-  //     fourth_pic: require("../../images/cherry4.jpg"),
-  //   },
+  resultCount: 50,
+  results: [
+    {
+      ItemNo: 1,
+      kind: "fruit",
+      FruitName: "Australian Cherry",
+      fruitPrice: "$5",
+      FruitImg1:
+        "https://agfstorage.blob.core.windows.net/misc/FP_com/2019/01/29/AnticoCherries2.jpg",
+      FruitImg4:
+        "https://lh3.googleusercontent.com/proxy/d5R9_McUemGLseso5kpNZzbAsa1fQ2u2ltk-v9mYn6ajCoyEIPiFCaXQdNuVfSn6cNhM2v-sAymVntSHqC1k0OABjAmsYKK4oEcSPm-AaYR_kZ6pj26K-c7KREY6hwk",
+      FruitImg3:
+        "https://www.masitrading.com/wp-content/uploads/2019/12/cherries.jpg",
+    },
+    {
+      ItemNo: 2,
+      kind: "fruit",
+      FruitName: "British Jackfruit",
+      fruitPrice: "$5",
+      FruitImg1:
+        "https://www.eckes-granini.com/fileadmin/eckesgranini/eckes-granini.com/05-best-of-fruit/fruechte/titelbilder/fruechte-titelbilder-kirsche.jpg",
+      FruitImg4:
+        "https://www.thespruceeats.com/thmb/ZnV1KFgs9UHKzDy1p8uMEZeALM4=/3004x3004/smart/filters:no_upscale()/MaraschinoCherries-5bce106546e0fb0051b70a88.jpg",
+      FruitImg3:
+        "https://www.loginradius.com/blog/async/static/d769a0cd91a6d5d0e55dde8cd498b324/701ee/cherrypck.jpg",
+    },
+    {
+      ItemNo: 3,
+      kind: "fruit",
+      FruitName: "Thai Cherry",
+      fruitPrice: "$5",
+      FruitImg1:
+        "https://www.eckes-granini.com/fileadmin/eckesgranini/eckes-granini.com/05-best-of-fruit/fruechte/titelbilder/fruechte-titelbilder-kirsche.jpg",
+      FruitImg4:
+        "https://www.thespruceeats.com/thmb/ZnV1KFgs9UHKzDy1p8uMEZeALM4=/3004x3004/smart/filters:no_upscale()/MaraschinoCherries-5bce106546e0fb0051b70a88.jpg",
+      FruitImg3:
+        "https://www.loginradius.com/blog/async/static/d769a0cd91a6d5d0e55dde8cd498b324/701ee/cherrypck.jpg",
+    },
+    {
+      ItemNo: 4,
+      kind: "fruit",
+      FruitName: "Asian Cherry",
+      fruitPrice: "$5",
+      FruitImg1:
+        "https://www.eckes-granini.com/fileadmin/eckesgranini/eckes-granini.com/05-best-of-fruit/fruechte/titelbilder/fruechte-titelbilder-kirsche.jpg",
+      FruitImg4:
+        "https://www.thespruceeats.com/thmb/ZnV1KFgs9UHKzDy1p8uMEZeALM4=/3004x3004/smart/filters:no_upscale()/MaraschinoCherries-5bce106546e0fb0051b70a88.jpg",
+      FruitImg3:
+        "https://www.loginradius.com/blog/async/static/d769a0cd91a6d5d0e55dde8cd498b324/701ee/cherrypck.jpg",
+    },
+    {
+      ItemNo: 5,
+      kind: "fruit",
+      FruitName: "Indian Apple",
+      fruitPrice: "$5",
+      FruitImg1:
+        "https://www.eckes-granini.com/fileadmin/eckesgranini/eckes-granini.com/05-best-of-fruit/fruechte/titelbilder/fruechte-titelbilder-kirsche.jpg",
+      FruitImg4:
+        "https://www.thespruceeats.com/thmb/ZnV1KFgs9UHKzDy1p8uMEZeALM4=/3004x3004/smart/filters:no_upscale()/MaraschinoCherries-5bce106546e0fb0051b70a88.jpg",
+      FruitImg3:
+        "https://www.loginradius.com/blog/async/static/d769a0cd91a6d5d0e55dde8cd498b324/701ee/cherrypck.jpg",
+    },
+    {
+      ItemNo: 6,
+      kind: "fruit",
+      FruitName: "Thai DragonFruit",
+      fruitPrice: "$5",
+      FruitImg1:
+        "https://www.eckes-granini.com/fileadmin/eckesgranini/eckes-granini.com/05-best-of-fruit/fruechte/titelbilder/fruechte-titelbilder-kirsche.jpg",
+      FruitImg4:
+        "https://www.thespruceeats.com/thmb/ZnV1KFgs9UHKzDy1p8uMEZeALM4=/3004x3004/smart/filters:no_upscale()/MaraschinoCherries-5bce106546e0fb0051b70a88.jpg",
+      FruitImg3:
+        "https://www.loginradius.com/blog/async/static/d769a0cd91a6d5d0e55dde8cd498b324/701ee/cherrypck.jpg",
+    },
+    {
+      ItemNo: 7,
+      kind: "fruit",
+      FruitName: "Indian Grapes",
+      fruitPrice: "$5",
+      FruitImg1:
+        "https://www.eckes-granini.com/fileadmin/eckesgranini/eckes-granini.com/05-best-of-fruit/fruechte/titelbilder/fruechte-titelbilder-kirsche.jpg",
+      FruitImg4:
+        "https://www.thespruceeats.com/thmb/ZnV1KFgs9UHKzDy1p8uMEZeALM4=/3004x3004/smart/filters:no_upscale()/MaraschinoCherries-5bce106546e0fb0051b70a88.jpg",
+      FruitImg3:
+        "https://www.loginradius.com/blog/async/static/d769a0cd91a6d5d0e55dde8cd498b324/701ee/cherrypck.jpg",
+    },
+    {
+      ItemNo: 8,
+      kind: "fruit",
+      FruitName: "American Pears",
+      fruitPrice: "$5",
+      FruitImg1:
+        "https://www.eckes-granini.com/fileadmin/eckesgranini/eckes-granini.com/05-best-of-fruit/fruechte/titelbilder/fruechte-titelbilder-kirsche.jpg",
+      FruitImg4:
+        "https://www.thespruceeats.com/thmb/ZnV1KFgs9UHKzDy1p8uMEZeALM4=/3004x3004/smart/filters:no_upscale()/MaraschinoCherries-5bce106546e0fb0051b70a88.jpg",
+      FruitImg3:
+        "https://www.loginradius.com/blog/async/static/d769a0cd91a6d5d0e55dde8cd498b324/701ee/cherrypck.jpg",
+    },
+    {
+      ItemNo: 9,
+      kind: "fruit",
+      FruitName: "Spanish Cherry",
+      fruitPrice: "$5",
+      FruitImg1:
+        "https://www.eckes-granini.com/fileadmin/eckesgranini/eckes-granini.com/05-best-of-fruit/fruechte/titelbilder/fruechte-titelbilder-kirsche.jpg",
+      FruitImg4:
+        "https://www.thespruceeats.com/thmb/ZnV1KFgs9UHKzDy1p8uMEZeALM4=/3004x3004/smart/filters:no_upscale()/MaraschinoCherries-5bce106546e0fb0051b70a88.jpg",
+      FruitImg3:
+        "https://www.loginradius.com/blog/async/static/d769a0cd91a6d5d0e55dde8cd498b324/701ee/cherrypck.jpg",
+    },
+  ],
 };
 
 export default DummyData;
